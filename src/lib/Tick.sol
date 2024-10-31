13de96cf272d7c8776ebe6009c6fd53152b6f662
X-spec7 <mercury.spec77@gmail.com>
2024-11-01 04:37:26 +0900

    feat: complete constructor

--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import './TickMath.sol';
+
+// Q: max liquidity calculated evenly?
 library Tick {
     function tickSpacingToMaxLiquidityPerTick(
         int24 tickSpacing
     ) internal pure returns (uint128) {
-      // TODO: implement
+      int24 mminTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+      int24 mmaxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+      uint24 numTicks = uint24((mmaxTick - mminTick) / tickSpacing) + 1;
+      return type(uint128).max / numTicks;
     }
 }
