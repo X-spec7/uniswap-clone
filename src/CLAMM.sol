5e54512f70a565921a4bbb5cbbffa4c9f56676ce
X-spec7 <mercury.spec77@gmail.com>
2024-11-01 05:15:28 +0900

    feat: create initialize function of CLAMM

--- src/CLAMM.sol
+++ src/CLAMM.sol
@@ -1,27 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import './lib/Tick.sol';
+import "./lib/Tick.sol";
 
 contract CLAMM {
-  address public immutable token0;
-  address public immutable token1;
-  uint24 public immutable fee;
-  int24 public immutable tickSpacing;
+    address public immutable token0;
+    address public immutable token1;
+    uint24 public immutable fee;
+    int24 public immutable tickSpacing;
 
-  uint128 public immutable maxLiquidityPerTick;
+    uint128 public immutable maxLiquidityPerTick;
 
-  constructor(
-    address _token0,
-    address _token1,
-    uint24 _fee,
-    int24 _tickSpacing
-  ) {
-    token0 = _token0;
-    token1 = _token1;
-    fee = _fee;
-    tickSpacing = _tickSpacing;
+    struct Slot0 {
+        // the current price
+        uint160 sqrtPriceX96;
+        // the current tick
+        int24 tick;
+        // whether the pool is locked
+        bool unlocked;
+    }
 
-    maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
-  }
+    Slot0 public slot0;
+
+    constructor(
+        address _token0,
+        address _token1,
+        uint24 _fee,
+        int24 _tickSpacing
+    ) {
+        token0 = _token0;
+        token1 = _token1;
+        fee = _fee;
+        tickSpacing = _tickSpacing;
+
+        maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(
+            _tickSpacing
+        );
+    }
+
+    function initialize(uint160 sqrtPriceX96) external {
+      require(slot0.sqrtPriceX96 == 0, "already initialized");
+
+      int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
+
+      slot0 = Slot0({
+        sqrtPriceX96: sqrtPriceX96,
+        tick: tick,
+        unlocked: true
+      });
+    }
 }
