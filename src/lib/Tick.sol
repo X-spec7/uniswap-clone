f132c1325f5094df2b29365dbff6ef380ade76e6
X-spec7 <mercury.spec77@gmail.com>
2024-11-01 03:35:12 +0900

    feat: create constructor of CLAMM

---
+++ src/lib/Tick.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+library Tick {
+    function tickSpacingToMaxLiquidityPerTick(
+        int24 tickSpacing
+    ) internal pure returns (uint128) {
+      // TODO: implement
+    }
+}
